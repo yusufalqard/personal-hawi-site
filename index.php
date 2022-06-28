902b69b9c7655763e1e29dc0d78e188496acb206
hawiqard <yusuf.alqard@gmail.com>
2022-06-28 14:05:07 +0700

    add html

--- index.php
+++ index.php
@@ -2,19 +2,3 @@
     $title = "Index";
     include 'include/header.php'
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Hawi Personal Site - HOME</title>
-</head>
-
-<body>
-    
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-
-        <h1>Hallo Pengunjung</h1>
-        <p>Selamat datang di Hawi Minimarket, Selamat berbelanja</p>
-    
-</body>
-</html>

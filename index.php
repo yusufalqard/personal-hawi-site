7b96f9ec3e3051937ab279e4a26fd3b3c57b7ebf
hawiqard <yusuf.alqard@gmail.com>
2022-06-28 10:12:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php 
+<?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hawi Personal Site - HOME</title>
+</head>
+
+<body>
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+        <h1>Hallo Pengunjung</h1>
+        <p>Selamat datang di Hawi Minimarket, Selamat berbelanja</p>
+    
+</body>
+</html>
